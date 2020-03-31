f4945b62973e808b7437f5247b44c8115f745bc3
Mauricio Martins <mauricio@economizebr.com>
2020-03-31 20:05:44 -0300

    task

--- tasks/src/CommonStyles.js
+++ tasks/src/CommonStyles.js
@@ -2,5 +2,7 @@ export default {
   fontFamily: 'Lato',
   colors: {
     secondary: '#FFF',
+    mainText: '#222',
+    subText: '#555',
   },
 };
--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import commonStyles from '../CommonStyles';
+
+import moment from 'moment';
+import 'moment/locale/pt-br';
 
 export default props => {
+  const doneOrNotStyle =
+    props.doneAt != null ? {textDecorationLine: 'line-through'} : {};
+
+  const date = props.doneAt ? props.doneAt : props.estimateAt;
+  const formattedDate = moment(date)
+    .locale('pt-br')
+    .format('ddd, D [de] MMMM');
+
   return (
     <View style={styles.container}>
       <View style={styles.checkContainer}>{getCheckView(props.doneAt)}</View>
       <View>
-        <Text>{props.desc}</Text>
-        <Text>{props.estimateAt + ''}</Text>
+        <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+        <Text style={styles.date}>{formattedDate + ''}</Text>
       </View>
     </View>
   );
@@ -18,13 +30,13 @@ function getCheckView(doneAt) {
   if (doneAt != null) {
     return (
       <View>
-        <Text>Opa</Text>
+        <Text style={styles.done}>
+          <Icon style={styles.icon} name="check" />
+        </Text>
       </View>
     );
   } else {
-    return (
-      <View style={styles.pending}></View>
-    );
+    return <View style={styles.pending} />;
   }
 }
 
@@ -45,5 +57,27 @@ const styles = StyleSheet.create({
     borderRadius: 13,
     borderWidth: 1,
     borderColor: '#555',
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: '#4D7031',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    fontSize: 20,
+    color: '#FFF',
+  },
+  desc: {
+    fontFamily: commonStyles.colors.fontFamily,
+    color: commonStyles.colors.mainText,
+    fontSize: 15,
+  },
+  date: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.subText,
+    fontSize: 12,
   },
 });
